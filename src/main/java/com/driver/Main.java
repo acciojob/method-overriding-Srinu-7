7bafa12e6be51891214c86e54af9752531a0eb34
Srinu <srinumaka9908@gmail.com>
2024-05-31 21:45:39 +0530

    committing my final changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B obj1 = new B();
-        System.out.println(obj1.meth());
-        A obj2 = new A();
-        System.out.println(obj2.meth());
+        B b = new B();
+        System.out.println(b.meth());
+        A a = new A();
+        System.out.println(agit .meth());
     }
 }
